83457142db24bec9f52c479d737b30b9482271d4
jombi <jombi@duck.com>
2024-08-26 00:44:20 +0900

    feat :: room entity

---
+++ src/main/kotlin/com/project/nanuriserver/domain/room/domain/entity/RoomEntity.kt
@@ -0,0 +1,24 @@
+package com.project.nanuriserver.domain.room.domain.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import java.util.UUID
+
+@Entity(name = "tb_room")
+class RoomEntity(
+    @Column(name = "user_first_id", nullable = false, updatable = false)
+    val userFirst: String,
+
+    @Column(name = "user_second_id", nullable = false, updatable = false)
+    val userSecond: String,
+
+    @Column(nullable = true)
+    val title: String? = null,
+
+    @Id
+    @Column(name = "id", nullable = false)
+    val _id: UUID? = null,
+) {
+    val id get() = _id!!
+}
