5c8f7c24d2a747a43ded1da72caadda82640171f
jombi <jombi@duck.com>
2024-08-26 11:38:15 +0900

    chore :: remove post fetch in user (moved to PostController.kt)

--- src/main/kotlin/com/project/nanuriserver/domain/user/client/api/UserController.kt
+++ src/main/kotlin/com/project/nanuriserver/domain/user/client/api/UserController.kt
@@ -1,6 +1,5 @@
 package com.project.nanuriserver.domain.user.client.api
 
-import com.project.nanuriserver.domain.post.client.dto.Post
 import com.project.nanuriserver.domain.user.client.dto.User
 import com.project.nanuriserver.domain.user.service.UserService
 import com.project.nanuriserver.global.common.dto.response.BaseResponseData
@@ -24,13 +23,4 @@ class UserController(
             message = "유저 프로필 조회 성공",
             data = userService.getUser())
     }
-
-    @GetMapping("/my-post")
-    @Operation(summary = "내 게시글 조회")
-    fun getMyPost(): BaseResponseData<List<Post>> {
-        return BaseResponseData.ok(
-            message = "나의 게시물 불러오기 성공",
-            data = userService.findMyPost())
-    }
-
 }
