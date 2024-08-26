38a3ad35fec42fad56e0dec8215fb6ac10d260b6
jombi <jombi@duck.com>
2024-08-26 11:39:51 +0900

    feat :: Safe String casting in enum things

--- src/main/kotlin/com/project/nanuriserver/domain/post/domain/enum/Status.kt
+++ src/main/kotlin/com/project/nanuriserver/domain/post/domain/enum/Status.kt
@@ -1,7 +1,16 @@
 package com.project.nanuriserver.domain.post.domain.enum
 
+import com.project.nanuriserver.domain.post.exception.InvalidStatusException
+
 enum class Status {
 
-    PROGRESS, DONE
+    PROGRESS, DONE;
 
+    companion object {
+        fun StatusSafe(status: String) = try {
+            valueOf(status)
+        } catch (e: IllegalArgumentException) {
+            throw InvalidStatusException
+        }
+    }
 }
