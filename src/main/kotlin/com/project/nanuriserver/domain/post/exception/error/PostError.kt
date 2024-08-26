c2364da6a43cfae5689190b2b13e9e17fa2e112e
jombi <jombi@duck.com>
2024-08-26 11:40:22 +0900

    feat :: add Exception kinds

--- src/main/kotlin/com/project/nanuriserver/domain/post/exception/error/PostError.kt
+++ src/main/kotlin/com/project/nanuriserver/domain/post/exception/error/PostError.kt
@@ -6,5 +6,7 @@ import org.springframework.http.HttpStatus
 enum class PostError(override val status: HttpStatus, override val message: String) : ErrorProperty {
 
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."),
+    INVALID_CATEGORY(HttpStatus.BAD_REQUEST, "알맞지 않은 게시글 카테고리입니다."),
+    INVALID_STATUS(HttpStatus.BAD_REQUEST, "알맞지 않은 게시글 상태입니다."),
 
 }
--- src/main/kotlin/com/project/nanuriserver/global/exception/error/ErrorCode.kt
+++ src/main/kotlin/com/project/nanuriserver/global/exception/error/ErrorCode.kt
@@ -8,6 +8,7 @@ enum class ErrorCode(
 ) : ErrorProperty {
 
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    ACCESS_DENIED(HttpStatus.BAD_REQUEST, "접근 요청이 거부되었습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "리소스를 찾을 수 없습니다.");
 
 }
