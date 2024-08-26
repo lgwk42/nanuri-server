d62c373f81a4594a21ee2bad45a46bb30cfd4095
jombi <jombi@duck.com>
2024-08-26 11:37:23 +0900

    chore :: remove unused lombok functions

--- src/main/kotlin/com/project/nanuriserver/domain/auth/service/response/RefreshTokenResponse.kt
+++ src/main/kotlin/com/project/nanuriserver/domain/auth/service/response/RefreshTokenResponse.kt
@@ -1,9 +1,7 @@
 package com.project.nanuriserver.domain.auth.service.response
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import lombok.Builder
 
-@Builder
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 data class RefreshTokenResponse(
     val accessToken: String
--- src/main/kotlin/com/project/nanuriserver/global/common/entity/BaseEntity.kt
+++ src/main/kotlin/com/project/nanuriserver/global/common/entity/BaseEntity.kt
@@ -3,17 +3,14 @@ package com.project.nanuriserver.global.common.entity
 import com.fasterxml.jackson.annotation.JsonFormat
 import jakarta.persistence.EntityListeners
 import jakarta.persistence.MappedSuperclass
-import lombok.experimental.SuperBuilder
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
-@SuperBuilder
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class BaseEntity {
-
     @CreatedDate
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     protected var createdDateTime: LocalDateTime? = null
